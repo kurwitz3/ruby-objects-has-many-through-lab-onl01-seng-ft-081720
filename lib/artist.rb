9f2d57e967975b0451d83d371684a5ff1b749a60
Richard Kurwitz <kurwitz3@aol.com>
2020-09-02 15:13:21 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,28 +1,32 @@
 require 'pry'
 class Artist 
+
 attr_accessor :name,:song
+
 @@all = [] 
+
 def initialize(name)
-@name = name   
+ @name = name   
   @@all << self 
 end 
+
 def self.all 
   @@all 
 end 
+
 def songs 
   Song.all.select {|song| song.artist == self }
-
 end 
+
 def new_song(name,genre)
- Song.new(name,self,genre )
-  end
-  
+  Song.new(name,self,genre )
+end
   
   def genres
     new = []
-    Song.all.select do |x|
+     Song.all.select do |x|
        new << x.genre 
+     end
+    new 
   end
-  new 
-end
 end 
